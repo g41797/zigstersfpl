cfebce1b473bd1a2d828c359a9e7c436cb5d62a1
g41797 <g41797@gmail.com>
2024-09-10 13:55:55 +0300

    Add test

--- src/main.zig
+++ src/main.zig
@@ -1,29 +1,30 @@
 const std       = @import("std");
 const String    = []const u8;
-const Strings    = []String;
+const Strings   = []String;
 const ArrayList = std.ArrayList;
 const Lines     = ArrayList([]u8);
 
 pub fn main() !void {
+    try run();
+    return;
+}
+
+pub fn run() !void {
 
     var gpa         = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
-    defer {
-        _ = gpa.deinit();
-    }
+        defer {
+            _ = gpa.deinit();
+        }
 
     const cwd_path      = std.fs.cwd().realpathAlloc(allocator, ".") catch |err| return err;
-        defer allocator.free(cwd_path);
+    defer allocator.free(cwd_path);
 
-    var paths   = [_]String{ cwd_path, "src/firstlang.log" };
+    var paths   	= [_]String{ cwd_path, "src/firstlang.log" };
     const file_path     = try std.fs.path.join(allocator, &paths);
-        defer allocator.free(file_path);
-
-    std.debug.print("\n\n", .{});
+    defer allocator.free(file_path);
 
     _ = try process(allocator, file_path);
-
-    std.debug.print("\n\n", .{});
 
     return;
 }
@@ -32,9 +33,9 @@ pub fn process(allocator: std.mem.Allocator, file_path: []const u8) !void {
 
     const strings: Strings  = try collect(allocator, file_path);
         defer {
-            for (strings) |str| {allocator.free(str);}
+            for (strings) |str| {allocator.free(str );}
             allocator.free(strings);
-    }
+    	}
 
     const stdout_file   = std.io.getStdOut().writer();
     var bw              = std.io.bufferedWriter(stdout_file);
@@ -59,7 +60,7 @@ pub fn collect(allocator: std.mem.Allocator, file_path: []const u8) !Strings {
         }
 
     var file = try std.fs.cwd().openFile(file_path, .{});
-    defer file.close();
+    	defer file.close();
 
     var buf_reader      = std.io.bufferedReader(file.reader());
     var in_stream       = buf_reader.reader();
@@ -70,17 +71,13 @@ pub fn collect(allocator: std.mem.Allocator, file_path: []const u8) !Strings {
     //--------------------------------------------------------------------------------
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
             const lang_info = try std.fmt.allocPrint(allocator, "{s}", .{line});
-            errdefer allocator.free(lang_info);
+            	errdefer allocator.free(lang_info);
             try lines.append(lang_info);
     }
 
     return lines.toOwnedSlice();
 }
 
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+test "run test" {
+    try run();
 }
--- src/root.zig
+++ src/root.zig
@@ -1,5 +1,6 @@
 const std       = @import("std");
 const testing   = std.testing;
+const main      = @import("main.zig");
 
 test {
     @import("std").testing.refAllDecls(@This());
