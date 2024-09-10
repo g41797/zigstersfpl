349ecaef755e9da4c6b5e28dc8b0d6156c4c6cde
g41797 <g41797@gmail.com>
2024-09-10 13:26:24 +0300

    Add reading file (line by line) to the memory

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 [![CI](https://github.com/g41797/zigstersfpl/actions/workflows/ci.yml/badge.svg)](https://github.com/g41797/zigstersfpl/actions/workflows/ci.yml)
 
-Repository for my [Reddit post](https://www.reddit.com/r/Zig/comments/1fazdl9/what_was_your_first_programming_language/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button)
+Repository for [Reddit post](https://www.reddit.com/r/Zig/comments/1fazdl9/what_was_your_first_programming_language/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button)
+
+
 
--- src/main.zig
+++ src/main.zig
@@ -1,36 +1,82 @@
-const std = @import("std");
+const std       = @import("std");
+const String    = []const u8;
+const Strings    = []String;
+const ArrayList = std.ArrayList;
+const Lines     = ArrayList([]u8);
 
 pub fn main() !void {
 
-    // Get an allocator.
     var gpa         = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
     defer {
         _ = gpa.deinit();
     }
 
-    var info = try std.fs.cwd().openFile("src/firstlang.log", .{});
-    defer info.close();
+    const cwd_path      = std.fs.cwd().realpathAlloc(allocator, ".") catch |err| return err;
+        defer allocator.free(cwd_path);
 
-    std.debug.print("Before processing.\n\n", .{});
+    var paths   = [_]String{ cwd_path, "src/firstlang.log" };
+    const file_path     = try std.fs.path.join(allocator, &paths);
+        defer allocator.free(file_path);
+
+    std.debug.print("\n\n", .{});
+
+    _ = try process(allocator, file_path);
+
+    std.debug.print("\n\n", .{});
+
+    return;
+}
+
+pub fn process(allocator: std.mem.Allocator, file_path: []const u8) !void {
+
+    const strings: Strings  = try collect(allocator, file_path);
+        defer {
+            for (strings) |str| {allocator.free(str);}
+            allocator.free(strings);
+    }
 
     const stdout_file   = std.io.getStdOut().writer();
     var bw              = std.io.bufferedWriter(stdout_file);
     const out           = bw.writer();
 
-    _ = try process(allocator, info, out);
+    for(strings) |string| {
+        try out.print("{s}\n", .{string});
+    }
 
     try bw.flush();
 
-    std.debug.print("After processing.\n\n", .{});
-
     return;
 }
 
-pub fn process(_: std.mem.Allocator, _: std.fs.File, _: anytype) !void {
+pub fn collect(allocator: std.mem.Allocator, file_path: []const u8) !Strings {
 
-    return;
+    var lines   = Lines.init(allocator);
+
+        defer {
+            for (lines.items) |l| {allocator.free(l);}
+            lines.deinit();
+        }
+
+    var file = try std.fs.cwd().openFile(file_path, .{});
+    defer file.close();
+
+    var buf_reader      = std.io.bufferedReader(file.reader());
+    var in_stream       = buf_reader.reader();
+    var buf: [256]u8    = undefined;
+
+    //--------------------------------------------------------------------------------
+    // https://ziggit.dev/t/understanding-how-to-properly-append-to-an-arraylist/339/2
+    //--------------------------------------------------------------------------------
+    while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+            const lang_info = try std.fmt.allocPrint(allocator, "{s}", .{line});
+            errdefer allocator.free(lang_info);
+            try lines.append(lang_info);
+    }
+
+    return lines.toOwnedSlice();
 }
+
 
 test "simple test" {
     var list = std.ArrayList(i32).init(std.testing.allocator);
